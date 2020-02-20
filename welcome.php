bd54dd1d17a85e1ca00e44bd68368ee74c7c5150
bwolmarans <itsbrett@gmail.com>
2020-02-20 11:40:39 -0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -6,7 +6,10 @@
       $myusername = $_SESSION['username'];
       $mypassword = $_SESSION['password'];
       $myanimal = $_POST['animal'];
+      $cc = $_POST['cc'];
       $sql = "UPDATE users SET animal = '$myanimal' WHERE username = '$myusername'";
+      echo 'sql: ' . $sql . '<br><br>';
+      $sql = "UPDATE users SET creditcard = '$cc' WHERE username = '$myusername'";
       echo 'sql: ' . $sql . '<br><br>';
 
       if (mysqli_query($conn, $sql)) {
@@ -25,7 +28,8 @@
    <body>
       <h1>Welcome <?php echo $login_session; ?> !</h1>
       <form action = "" method = "post">
-      <label>What is your fave animal? (We store it in your user record as a 2nd factor):</label><input type = "text" name = "animal"/><br/><br/>
+      <label>What is your fave animal? (We store it in your record as a 2nd factor):</label><input type = "text" name = "animal"/><br/><br/>
+      <label>What is your creditcard? (We ask you to put it in, just because):</label><input type = "text" name = "cc"/><br/><br/>
       <input type = "submit" value = " Submit "/><br />
       </form>
       <h2><a href = "logout.php">Sign Out</a></h2>
