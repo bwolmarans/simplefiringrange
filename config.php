db2ac5f68c700effed72d6e569cbc97e2d771b0f
bwolmarans <itsbrett@gmail.com>
2020-06-26 11:47:27 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
    define('DB_SERVER', '127.0.0.1');
-   define('DB_USERNAME', 'root');
+   define('DB_USERNAME', 'admin');
    define('DB_PASSWORD', 'hello123');
    define('DB_DATABASE', 'brett');
    $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
